d81ea99b1ad1c8a9f60636dbe6de1ec59e791e28
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2022-11-13 23:22:57 -0800

    Add hardcoded functions and their derivatives

--- Main.java
+++ Main.java
@@ -9,7 +9,27 @@
  {
     public static void main(String[] args)
     {
-        
+
+    }
+
+    private double f(double x)
+    {
+        return (2 * Math.pow(x, 3)) - (11.7 * Math.pow(x, 2)) + (17.7 * x) -5;
+    }
+
+    private double fPrime(double x)
+    {
+        return (6 * Math.pow(x, 2)) - (23.4 * x) + 17.7;
+    }
+
+    private double g(double x)
+    {
+        return x + 10 - (x * Math.cosh(50/x));
+    }
+
+    private double gPrime(double x)
+    {
+        return 1 + (50 * Math.sinh(50/x) / x) - Math.cosh(50/x);
     }
  
  }
