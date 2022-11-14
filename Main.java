da031a0de80325679601de6c174d985b338de5b0
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2022-11-13 23:05:43 -0800

    Create main file

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+ * Project 3
+ * By Janista Gitbumrungsin
+ * CS3010.02 Fall 2022
+ * Dr. Lajpat Rai Raheja
+ */
+
+ class Main
+ {
+    public static void main(String[] args)
+    {
+        
+    }
+ 
+ }
